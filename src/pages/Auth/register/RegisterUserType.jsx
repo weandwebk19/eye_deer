a0baed4b4caf333c231e05d43a23fbcb021ea8a0
Nguyen Nhat Tho LE <88726462+croissain@users.noreply.github.com>
2022-11-05 12:54:58 +0700

    resolve conflict 1

--- src/pages/Auth/register/RegisterUserType.jsx
+++ src/pages/Auth/register/RegisterUserType.jsx
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import { useQuery } from "react-query";
+=======
+import { useState, useEffect } from "react";
+import { QueryClient,useQueryClient, QueryClientProvider, useQuery } from "react-query";
+>>>>>>> parent of d1d360f (cleaning some comments)
 import axios from "axios";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 
@@ -13,18 +18,26 @@ const RegisterUserType = () => {
   const { state } = useLocation();
 
   // const queryClient = new QueryClient();
-  const useWorkplaces = () => {
-    return useQuery(["workplace"], async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_SERVERBASEURL}${process.env.REACT_APP_SERVERPORT}/workplace/workplaces`
-      );
-      return data;
-    });
-  };
+  const useWorkplaces = ()=>{
+    return useQuery(["workplace"], async()=>{
+      const {data} = await axios.get(`${process.env.REACT_APP_SERVERBASEURL}${process.env.REACT_APP_SERVERPORT}/workplace/workplaces`);
+      return data
+    })
+  }
   const { isLoading, error, data, isFetching } = useWorkplaces();
 
   if (isLoading) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `${process.env.REACT_APP_SERVERBASEURL}${process.env.REACT_APP_SERVERPORT}/workplace/workplaces`
+  //     )
+  //     .then((response) => {
+  //       setWorkplaces(response.data);
+  //     });
+  // }, []);
 
   const handleNavigate = () => {
     const path = "/register/form";
@@ -32,47 +45,50 @@ const RegisterUserType = () => {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box className="register-container" sx={{ flexGrow: 1 }}>
-        <main>
-          {/* Hero unit */}
-          <div>
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Describe your workplace
-            </Typography>
-          </div>
-          {/* End hero unit */}
-        </main>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          {data.map((workplace, i) => {
-            return (
-              <Grid item xs={12} sm={6} md={2.5} key={i}>
-                <Link to="/register/form" state={{ workplace: workplace.id }}>
-                  <RegisterCard cardItem={workplace} />
-                </Link>
-              </Grid>
-            );
-          })}
-          <div>{isFetching ? "Updating..." : ""}</div>
-        </Grid>
-        <Button variant="contained" onClick={handleNavigate}>
-          Other
-        </Button>
-      </Box>
-    </LocalizationProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box className="register-container" sx={{ flexGrow: 1 }}>
+          <main>
+            {/* Hero unit */}
+            <div>
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Describe your workplace
+              </Typography>
+            </div>
+            {/* End hero unit */}
+          </main>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+              {data.map((workplace, i) => {
+                return (
+                  <Grid item xs={12} sm={6} md={2.5} key={i}>
+                    <Link
+                    to="/register/form"
+                    state={{ workplace: workplace.id }}
+                    >
+                      <RegisterCard cardItem={workplace} />
+                    </Link>
+                  </Grid>
+                );
+              })}
+              <div>{isFetching ? "Updating..." : ""}</div>
+          </Grid>
+          <Button variant="contained" onClick={handleNavigate}>
+            Other
+          </Button>
+        </Box>
+      </LocalizationProvider>
   );
 };
 
