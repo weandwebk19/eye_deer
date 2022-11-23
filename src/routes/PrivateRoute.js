aa1abaff9c72e1e5dd4d994110231c810ea6510e
Manh <k19weandweb@gmail.com>
2022-11-23 16:35:17 +0700

    Fix bug styledtab and private route

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,11 +1,10 @@
 import { useLoadPage } from '../hooks';
 
-const PrivateRoute = () => {
-    const { isLoading, error, data } = useLoadPage();
+const PrivateRoute = (props) => {
+    const { isLoading, error, data } = useLoadPage(props);
     if (isLoading) return "Loading...";
     if (error) return "An error has occurred: " + error.message;
 
     return data;
 }
-
 export default PrivateRoute;
