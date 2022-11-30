f511650332d99a47d64b8fd1c49bb16f3dbea031
Nguyen Nhat Tho LE <88726462+croissain@users.noreply.github.com>
2022-11-30 11:49:56 +0700

    update add member ui

--- src/components/Card/VisitCard.jsx
+++ src/components/Card/VisitCard.jsx
@@ -32,14 +32,14 @@ const menulist = [
   },
 ];
 
-const VisitCard = ({ variant, user }) => {
+const VisitCard = ({ variant, user, handleClick }) => {
   const [memberId, setMemberId] = useState(1);
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -49,44 +49,81 @@ const VisitCard = ({ variant, user }) => {
   };
 
   return (
-    <StyledCard variant="card-visit">
-      <Box sx={{ display: "flex", height: "100% !important" }}>
-        <CardActionArea
-          disableRipple
-          onClick={() => {
-            navigate(`${memberId}`);
-          }}
-        >
-          <CardContent sx={{ display: "flex" }}>
-            <Avatar
-              src={
-                user.picture ??
-                "https://res.cloudinary.com/dbaulxzoc/image/upload/v1669543514/WeAndWeb/bg-img-4_ujohe5.jpg"
-              }
-              sx={{ width: 64, height: 64, marginRight: "16px" }}
-            />
-            <Box sx={{ textOverflow: "ellipsis" }}>
-              <Typography variant="h6">
-                {`${user.firstName} ${user.lastName}`}
-              </Typography>
-              <Typography>{user.email}</Typography>
-              <Typography>{user.username}</Typography>
-            </Box>
-            {(() => {
-              if (variant === "no-morebutton") {
-                return "";
-              } else return <MoreButton menulist={menulist} />;
-            })()}
-          </CardContent>
-        </CardActionArea>
-      </Box>
-    </StyledCard>
+    <Box>
+      {(() => {
+        if (variant === "wide") {
+          return (
+            <StyledCard variant="card-visit" sx={{ height: "56px" }}>
+              <Box sx={{ display: "flex", height: "100% !important" }}>
+                <CardActionArea disableRipple onClick={handleClick}>
+                  <CardContent sx={{ display: "flex", p: 1 }}>
+                    <Avatar
+                      src={
+                        user.picture ??
+                        "https://res.cloudinary.com/dbaulxzoc/image/upload/v1669543514/WeAndWeb/bg-img-4_ujohe5.jpg"
+                      }
+                      sx={{ marginRight: "16px" }}
+                    />
+                    <Box sx={{}}>
+                      <Typography
+                        variant="body2"
+                        sx={{ textAlign: "start", fontWeight: "bold" }}
+                      >
+                        {`${user.firstName} ${user.lastName}`}
+                      </Typography>
+                      <Typography variant="caption">{user.email}</Typography>
+                      <span>・</span>
+                      <Typography variant="caption">{user.username}</Typography>
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
+              </Box>
+            </StyledCard>
+          );
+        } else
+          return (
+            <StyledCard variant="card-visit">
+              <Box sx={{ display: "flex", height: "100% !important" }}>
+                <CardActionArea
+                  disableRipple
+                  onClick={() => {
+                    handleClick(user.id);
+                  }}
+                >
+                  <CardContent sx={{ display: "flex" }}>
+                    <Avatar
+                      src={
+                        user.picture ??
+                        "https://res.cloudinary.com/dbaulxzoc/image/upload/v1669543514/WeAndWeb/bg-img-4_ujohe5.jpg"
+                      }
+                      sx={{ width: 64, height: 64, marginRight: "16px" }}
+                    />
+                    <Box sx={{ textOverflow: "ellipsis" }}>
+                      <Typography variant="h6">
+                        {`${user.firstName} ${user.lastName}`}
+                      </Typography>
+                      <Typography>{user.email}</Typography>
+                      <Typography>{user.username}</Typography>
+                    </Box>
+                    {(() => {
+                      if (variant === "no-morebutton") {
+                        return "";
+                      } else return <MoreButton menulist={menulist} />;
+                    })()}
+                  </CardContent>
+                </CardActionArea>
+              </Box>
+            </StyledCard>
+          );
+      })()}
+    </Box>
   );
 };
 
 VisitCard.propTypes = {
   variant: PropTypes.string,
   user: PropTypes.object,
+  handleClick: PropTypes.func.isRequired,
 };
 
 VisitCard.defaultProps = {
